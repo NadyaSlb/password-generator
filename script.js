089ed8cba084ede11b47f96248d89d041f07a48d
Nadya <hoperu1986@gmail.com>
2022-04-22 19:36:23 -0400

    add comments

--- script.js
+++ script.js
@@ -1,44 +1,6 @@
 // Assignment code here
-var criteria = function(){
-  var criteriaOption = window.prompt("Would you like to choose password criteria? Please enter one 1 for password length, 2 for character types, 3 for both, 4 for random ctiteria.");
-
-  criteriaOption = parseInt(criteriaOption);
-
-  switch(criteriaOption){
-    case 1:
-      passwordlength();
-      break;
-    case 2:
-      passwordCharacter();
-    case 3:
-      bothcriteria();
-    case 4:
-      randomcriteria();
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-      criteria();
-  }
-}
-var passwordlength = function(){
-
-  var length = window.prompt("Please, choose the length of your password. Enter the number from 8 to 128.");
-
-if (length === ""||length === null){
-  window.alert("You need to provide a valid answer! Please, try again.");
-
-  return passwordlength();
-
-}else if (length >= 8||length <= 128){
-  window.alert("The password length is " + length);
-
-}else{
-  window.alert("You need to provide a valid answer! Please, try again.");
- 
-  return passwordlength();
-}
-}
-
-//"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", ']", "^", "_", "`", "{", "|", "}", "~"//
+// Global variables
+var allcharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", ']", "^", "_", "`", "{", "|", "}", "~"];
 
 var lowercases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -48,31 +10,70 @@ var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var special = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", ']", "^", "_", "`", "{", "|", "}", "~"];
 
+// Functions
+//Generate password
+function generatePassword(){
+  var password = "";
+  var length = passwordlength();
+  var symbols = passwordCharacter();
+  for (var i = 0; i < length; i++){
+    var random = symbols[Math.floor(Math.random() * symbols.length)];
+    console.log(random);
+password += random;
+console.log(password);
+  }
+return password;
+}
+//Select length
+var passwordlength = function(){
+  var length = "";
 
+length = window.prompt("Please, choose the length of your password. Enter the number from 8 to 128.");
+
+if (length >= 8 && length <= 128){
+  window.alert("The password length is " + length);
+
+}else{
+  window.alert("You need to provide a valid answer! Please, try again.");
+ 
+  return passwordlength();
+}
+return length;
+}
+//Select characters
 var passwordCharacter = function(){
+  var symbols = [];
   window.alert("Please confirm at least one character type.");
+
   var confLow = window.confirm("Confirm if you want to include lowercase characters.");{
   if (confLow){
-    lowercases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  }else{
-    lowercases=[];
-  }
-  console.log(lowercases);
+   symbols = symbols.concat(lowercases);}
+  console.log(symbols);
 }
   var confUp = window.confirm("Confirm if you want to include uppercase characters.");{
     if (confUp){
-      arr1 = lowercases.concat(uppercase);
-    }else{
-      arr1 = lowercases;
-    }
-    console.log(arr1);
+  symbols = symbols.concat(uppercase);}
+    console.log(symbols);
   }
-  window.confirm("Confirm if you want to include numeric characters.");
-  window.confirm("Confirm if you want to include special characters.");
+
+  var confnum = window.confirm("Confirm if you want to include numeric characters.");{
+    if (confnum){
+      symbols = symbols.concat(numeric);}
+    console.log(symbols);
+  }
+
+  var confspec = window.confirm("Confirm if you want to include special characters.");{
+    if (confspec){
+      symbols = symbols.concat(special);}
+    console.log(symbols);
+  }
+
+  if (symbols.length === 0){
+    window.alert("You need to provide a valid answer! Please, try again.");
+    return passwordCharacter();
 }
-
-
-
+return symbols;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
